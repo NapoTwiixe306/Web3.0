91a54a9cc354099fd3ed025a6fbcd43ea610c73e
julien milants <julien.milants@gmail.com>
2022-01-21 20:22:49 +0100

    add client folder

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,9 @@
+import React, {useContext} from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
+import { TransactionContext } from "../context/TransactionContext";
 import { Loader } from "./";
 
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -17,18 +19,12 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
     />
   );
 const Welcome = () => {
-    const connectWallet = () => {
+    const { connectWallet, currentAccount } = useContext(TransactionContext);
     
-};  
+
 
 const handleSubmit = (e) => {
-    const { addressTo, amount, keyword, message } = formData;
 
-    e.preventDefault();
-
-    if (!addressTo || !amount || !keyword || !message) return;
-
-    sendTransaction();
   };
 
     
@@ -42,16 +38,20 @@ const handleSubmit = (e) => {
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                     Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
                      </p>
-                     <button
-                     type="button0"
-                     onClick={connectWallet}
-                     className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:br-[#2546bd]"
-                     >
-                    <AiFillPlayCircle className="text-white mr-2" />
-                    <p className="text-white text-base font-semibold">
-                        Connect Wallet
-                    </p>
-                     </button>
+                     {!currentAccount && (
+                        <button
+                        type="button0"
+                        onClick={connectWallet}
+                        className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:br-[#2546bd]"
+                        >
+                        <AiFillPlayCircle className="text-white mr-2" />
+                        <p className="text-white text-base font-semibold">
+                            Connect Wallet
+                        </p>
+                        </button>
+                     )}
+
+
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                         <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
                         Reliability
@@ -116,6 +116,8 @@ const handleSubmit = (e) => {
             </div>
         </div>
     );
-}
+};
+
+
 
 export default Welcome;
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress ='0x81C3a7f06A0A678d167853017Ca9F71eF08543e5';
